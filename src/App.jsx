77fd52add4386bc64c4fac3b2935782e86ece43e
leafhub <hnakayama@leafhub.me>
2019-05-29 14:17:16 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Blank from "./components/Blank";
+import List from "./components/List";
+import Grid4 from "./components/Grid4";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          < Redirect exact path = "/" to = "/Blank" />
+          < Route path = "/Blank" component = { Blank } />
+          < Route path = "/List" component = { List } />
+          < Route path = "/Grid4" component = { Grid4 } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
